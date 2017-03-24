3ffb0d3f3417f3d6246232c8e11e51ddb098b912
Henry Tao <hi@henrytao.me>
2017-03-24 21:57:39 +0700

    Added gulp taks

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp')
+const mocha = require('gulp-mocha')
+const nodemon = require('gulp-nodemon')
+const runSequence = require('run-sequence')
+
+gulp.task('server', () => {
+  nodemon({
+    script: './server.js',
+    ext: 'js json',
+    ignore: ['./test/'],
+    execMap: {
+      js: "node --harmony"
+    }
+  })
+})
+
+gulp.task('test', done => {
+  runSequence('test:unit', 'test:integration', done)
+})
+
+gulp.task('test:unit', () => {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'test'
+  gulp.src(['./test/unit/initialize.js', process.env.SPECS || './test/unit/**/*.spec.js'], {
+    read: false
+  })
+    .pipe(mocha({
+      harmony: true,
+      reporter: 'spec'
+    }))
+})
+
+gulp.task('test:integration', () => {
+  process.env.NODE_ENV = process.env.NODE_ENV || 'test'
+  gulp.src(['./test/integration/onInitialize.js', process.env.SPECS || './test/integration/**/*.spec.js'], {
+    read: false
+  })
+    .pipe(mocha({
+      harmony: true,
+      reporter: 'spec'
+    }))
+})
